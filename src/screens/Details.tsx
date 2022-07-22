e2f65ff8eb494df760a21290501629445f3b7e0d
andresdosantos <andres.dosantosbritoamaral@gmail.com>
2022-07-21 22:30:10 -0300

    feat(cardDetails): create cardDetails

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,22 +1,135 @@
+import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore'
 import { useRoute } from '@react-navigation/native'
-import { Text, VStack } from 'native-base'
+import { HStack, ScrollView, Text, useTheme, VStack } from 'native-base'
+import { useEffect, useState } from 'react'
+import firestore from '@react-native-firebase/firestore'
+import {
+  CircleWavyCheck,
+  Hourglass,
+  DesktopTower,
+  Clipboard,
+} from 'phosphor-react-native'
 
 import { Header } from '../components/Header'
+import { Loading } from '../components/Loading'
+import { CardDetails } from '../components/CardDetails'
+import { OrderProps } from '../components/Order'
+
+import { dateFormat } from '../utils/firestoreDateFormat'
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO'
+import { Input } from '../components/Input'
+import { Button } from '../components/Button'
 
 type RouteParams = {
   orderId: string
 }
 
+type OrderDetails = OrderProps & {
+  description: string
+  solution: string
+  closedAt: string
+}
+
 export function Details() {
   const route = useRoute()
+  const { colors } = useTheme()
 
   const { orderId } = route.params as RouteParams
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [solution, setSolution] = useState('')
+  const [order, setOrder] = useState<OrderDetails>({} as OrderDetails)
+
+  useEffect(() => {
+    firestore()
+      .collection<OrderFirestoreDTO>('orders')
+      .doc(orderId)
+      .get()
+      .then((doc) => {
+        const {
+          patrimony,
+          description,
+          status,
+          createdAt,
+          solution,
+          closedAt,
+        } = doc.data()
+
+        const closed = closedAt ? dateFormat(closedAt) : null
+
+        setOrder({
+          id: doc.id,
+          patrimony,
+          description,
+          status,
+          solution,
+          when: dateFormat(createdAt),
+          closedAt: closed,
+        })
+
+        setIsLoading(false)
+      })
+  }, [])
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <VStack flex={1} bg="gray.700">
       <Header title="Solicitação" />
 
-      <Text color="white">{orderId}</Text>
+      <HStack bg="gray.500" justifyContent="center" p={4}>
+        {order.status === 'closed' ? (
+          <CircleWavyCheck size={22} color={colors.green[300]} />
+        ) : (
+          <Hourglass size={22} color={colors.secondary[700]} />
+        )}
+
+        <Text
+          fontSize="sm"
+          color={
+            order.status === 'closed'
+              ? colors.green[300]
+              : colors.secondary[700]
+          }
+          ml={2}
+          textTransform="uppercase"
+        >
+          {order.status === 'closed' ? 'Finalizado' : 'Em andamento'}
+        </Text>
+      </HStack>
+
+      <ScrollView mx={5} showsVerticalScrollIndicator={false}>
+        <CardDetails
+          title="Equipamentos"
+          description={`Patrimônio ${order.patrimony}`}
+          icon={DesktopTower}
+          footer={order.when}
+        />
+
+        <CardDetails
+          title="Descrição do problema"
+          description={order.description}
+          icon={Clipboard}
+        />
+
+        <CardDetails
+          title="Solução"
+          icon={CircleWavyCheck}
+          footer={order.closedAt && `Encerrado em ${order.closedAt}`}
+        >
+          <Input
+            placeholder="Descrição da solução"
+            onChangeText={setSolution}
+            h={24}
+            textAlignVertical="top"
+            multiline
+          />
+        </CardDetails>
+      </ScrollView>
+
+      {!order.closedAt && <Button title="Encerrar solicitação" m={5} />}
     </VStack>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,16 +9,20 @@ import {
   Center,
 } from 'native-base'
 import { SignOut } from 'phosphor-react-native'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Alert } from 'react-native'
 import { ChatTeardropText } from 'phosphor-react-native'
 import { useNavigation } from '@react-navigation/native'
 import auth from '@react-native-firebase/auth'
+import firestore from '@react-native-firebase/firestore'
 
 import Logo from '../assets/logo_secondary.svg'
 import { Filter } from '../components/Filter'
 import { Order } from '../components/Order'
 import { Button } from '../components/Button'
+import { Loading } from '../components/Loading'
+
+import { dateFormat } from '../utils/firestoreDateFormat'
 
 import type { OrderProps, Status } from '../components/Order'
 
@@ -26,15 +30,9 @@ export function Home() {
   const { colors } = useTheme()
   const navigation = useNavigation()
 
+  const [isLoading, setIsLoading] = useState(true)
   const [statusSelected, setStatusSelected] = useState<Status>('open')
-  const [orders, setOrders] = useState<OrderProps[]>([
-    {
-      id: '123',
-      patrimony: '48948989',
-      status: 'closed',
-      when: '18/07/2022 às 17:48',
-    },
-  ])
+  const [orders, setOrders] = useState<OrderProps[]>([])
 
   const handleNewOrder = () => {
     navigation.navigate('register')
@@ -53,6 +51,32 @@ export function Home() {
         return Alert.alert('Sair', 'Não foi possível desconectar.')
       })
   }
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    const subscriber = firestore()
+      .collection('orders')
+      .where('status', '==', statusSelected)
+      .onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => {
+          const { patrimony, description, status, createdAt } = doc.data()
+
+          return {
+            id: doc.id,
+            patrimony,
+            description,
+            status,
+            createdAt,
+            when: dateFormat(createdAt),
+          }
+        })
+        setOrders(data)
+        setIsLoading(false)
+      })
+
+    return subscriber
+  }, [statusSelected])
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -82,7 +106,7 @@ export function Home() {
           alignItems="center"
         >
           <Heading color="gray.100">Meus chamados</Heading>
-          <Text color="gray.200">3</Text>
+          <Text color="gray.200">{orders.length}</Text>
         </HStack>
 
         <HStack space={3} mb={8}>
@@ -100,24 +124,28 @@ export function Home() {
           />
         </HStack>
 
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: 100 }}
-          ListEmptyComponent={() => (
-            <Center>
-              <ChatTeardropText color={colors.gray[300]} size={40} />
-              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
-                Você ainda não possui {'\n'} solicitações{' '}
-                {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
-              </Text>
-            </Center>
-          )}
-          data={orders}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
-          )}
-        />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 100 }}
+            ListEmptyComponent={() => (
+              <Center>
+                <ChatTeardropText color={colors.gray[300]} size={40} />
+                <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                  Você ainda não possui {'\n'} solicitações{' '}
+                  {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+                </Text>
+              </Center>
+            )}
+            data={orders}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+            )}
+          />
+        )}
 
         <Button title="Nova solicitação" onPress={handleNewOrder} />
       </VStack>
